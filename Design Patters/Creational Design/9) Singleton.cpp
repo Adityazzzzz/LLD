a52e4c2ae93b5363567d12e68fc6baa315707753
Aditya Singh <adityasinghrajawat2004@gmail.com>
2026-07-23 19:52:47 +0530

    Singleton

---
+++ Design Patters/Creational Design/9) Singleton.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+/*
+    Ensure only one object of a class exists and provide a global access point.
+
+    Real-life Example
+        Printer Spooler
+        Logger
+        Database Connection
+        Configuration Manager
+Only one instance should exist.
+
+*/
+
+#include <iostream>
+using namespace std;
+
+class Logger{
+private:
+    Logger(){}
+public:
+    static Logger& getInstance(){
+        static Logger obj;
+        return obj;
+    }
+    void log(){
+        cout<<"Logging...";
+    }
+};
+
+int main(){
+    Logger::getInstance().log();
+}
